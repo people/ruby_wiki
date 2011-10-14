efa2b49301d98d3a438a12b52a46814206b60879
people <people.github@gmail.com>
2011-10-13 21:04:41 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyWiki::Application.config.secret_token = '6bdcf05abffe5f8e726de9f2f76614e0d52c9c2c2f0d716b3981c0c00831e378c85be5ff8c98ea9303cad5da293aaf4e62456727c2636a23a4556c6425d5e6a0'
